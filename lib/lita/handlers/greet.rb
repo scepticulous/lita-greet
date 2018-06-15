430e3cb4151c04a29da0177adca4e59f52a1bbc5
Dennis Sivia <dennis.sivia@gmail.com>
2018-06-15 10:10:00 +0200

    Adds `moin` to greetings

--- lib/lita/handlers/greet.rb
+++ lib/lita/handlers/greet.rb
@@ -2,13 +2,13 @@ module Lita
   module Handlers
     class Greet < Handler
 
-      route(/^(((ahoy|he+l{2,}+o+|he+y+|hi+)+( there)*)|go{2,}d (morning|afternoon|evening|night|nite)|greetings|howdy|sup|yo)+[!1?]*((\s)+Regexp.new(robot.config.robot.name))*[!1?]*$/i, :say_hello)
+      route(/^(((ahoy|he+l{2,}+o+|he+y+|hi+)+( there)*)|go{2,}d (morning|afternoon|evening|night|nite)|greetings|howdy|moin|sup|yo)+[!1?]*((\s)+Regexp.new(robot.config.robot.name))*[!1?]*$/i, :say_hello)
       route(/^welcome (.+)/i, :welcome)
 
       def say_hello(response)
         return if response.user.name.empty?
         bot_response = response.message.body.gsub(/[1?!]+/, "").gsub(/(\s)*#{robot.config.robot.name}(\s)*/i, "")
-        response.reply "#{bot_response} #{(response.user.mention_name || response.user.name)}"
+        response.reply "#{bot_response} #{(response.user.name || response.user.mention_name)}"
       end
 
       def welcome(response)
